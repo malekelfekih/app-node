cd20bb70ccfd6d35d2920eda177560fc291d48a9
SoumayaArgoubi <soumaya.argoubi@esprit.tn>
2022-06-12 12:12:28 +0100

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const app = express()
+//step2
+const parkings =require('./parkings.json')
+
+//step4 //middleware
+app.use(express.json())
 
 app.get ('/parkings', (req,res)=> {
-res.send("Liste des parkings")
-
+//step1
+//res.send("Liste des parkings")
+//step2
+res.status(200).json(parkings)
+})
+ //step3
+app.get('/parkings/:id', (req,res) => {
+   const id = parseInt(req.params.id)
+   const parking = parkings.find(parking=>parking.id === id)
+   res.status(200).json(parking)
 })
 
+//step5 //post
+app.post('/parkings',(req,res) => {
+    parkings.push(req.body)
+    res.status(200).json(parkings)
+})
 
+//step0
 app.listen(8080, () => {
 console.log ('Serveur à l\'écoute')
 })
