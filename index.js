b5d02a22a4f9f6b5a658719ad87a1f519989755a
SoumayaArgoubi <soumaya.argoubi@esprit.tn>
2022-06-12 20:54:55 +0100

    Third commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const parkings =require('./parkings.json')
 //step4 //middleware
 app.use(express.json())
 
+//step1 //Get
 app.get ('/parkings', (req,res)=> {
 //step1
 //res.send("Liste des parkings")
@@ -22,6 +23,24 @@ app.get('/parkings/:id', (req,res) => {
 //step5 //post
 app.post('/parkings',(req,res) => {
     parkings.push(req.body)
+    res.status(200).json(parkings)
+})
+
+//step6 //PUT //vérifier l'emplacement
+app.put ('/parkings/:id', (req,res) => {
+    const id = parseInt (req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parking.name =req.body.name,
+    parking.city =req.body.city,
+    parking.type =req.body.type,
+    res.status(200).json(parking)
+})
+
+//step7 //Delete
+app.delete('/parkings/:id', (req,res)=> {
+    const id = parseInt(req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parkings.slice(parkings.indexOf(parking),1)
     res.status(200).json(parkings)
 })
 
